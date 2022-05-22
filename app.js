ca4bc6df94055884e0eb3aa9a5cf5cf4f3848467
Unis434 <unisk434@gmail.com>
2022-05-22 18:10:37 +0000

    changes made to port

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-const { fstat } = require('fs')
-const http = require('http')
-const port = 3000
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    fs.readFile('/src/index.html', function(error, data) {
-        if (error) {
-            res.writeHead(404)
-            res.write('Error: File not found')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })
-})
+const app = express()
+const port = 5000
+
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'src/css'))
+app.use('/img', express.static(__dirname + 'src/imgages'))
+app.use('/js', express.static(__dirname + 'src/js'))
+
+// Templating Engine
+app.set('app', './src/index')
+app.set('view engine', 'index')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Routes
+const newsRouter = require('./src/routes.spec')
+
+app.use('/', index.html)
+app.use('/article', newsRouter)
+
+// Listen on port 5000
+app.listen(port, () => console.log(`Listening on port ${port}`))
